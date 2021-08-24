39689125bdf90a8f438b0d3b6d02f64e999c0b97
JOSE MANUEL MEDINA CANO <josemedina224120@correo.itm.edu.co>
2021-08-23 23:19:17 -0500

    Adding Entity

---
+++ todoJose.Common/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace todoJose.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
